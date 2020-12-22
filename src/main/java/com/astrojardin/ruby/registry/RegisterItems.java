529ca7452ae98f83cd426c35fcadaf18db631690
BrandonFlorian <brandon.r.florian@gmail.com>
2020-12-22 16:39:46 -0500

    Armor pieces
    Adding armor pieces
    textures not showing up yet

--- src/main/java/com/astrojardin/ruby/registry/RegisterItems.java
+++ src/main/java/com/astrojardin/ruby/registry/RegisterItems.java
@@ -5,14 +5,11 @@ import com.astrojardin.ruby.armor.BaseArmor;
 import com.astrojardin.ruby.armor.RubyArmorMaterial;
 import com.astrojardin.ruby.block.RubyBlock;
 import com.astrojardin.ruby.tools.*;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.lwjgl.system.CallbackI;
 
 public class RegisterItems {
 
@@ -32,12 +29,9 @@ public class RegisterItems {
     Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_shovel"), new ShovelBase(new ToolMaterialRuby()));
     Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_hoe"), new HoeBase(new ToolMaterialRuby()));
 
-
-
-
-//    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_helmet"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.HEAD));
-//    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_chestplate"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.CHEST));
-//    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_leggings"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.LEGS));
-//    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_boots"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.FEET));
+    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_helmet"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.HEAD));
+    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_chestplate"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.CHEST));
+    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_leggings"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.LEGS));
+    Registry.register(Registry.ITEM, new Identifier(MODID, "ruby_boots"), new BaseArmor(RUBY_ARMOR, EquipmentSlot.FEET));
     }
 }
